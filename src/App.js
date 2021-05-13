849c0c17f45d23e92816f1fa8b67c227f53cd504
Kim <faeriekim3@gmail.com>
2021-05-13 17:23:11 +0100

    some progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import React from "react";
+import React, {useState} from "react";
 import { Cat} from "./Cat.js";
 
 const App = () => {
+  const [basketName, setBasketName] = useState([]);
+  const [basketPrice, setBasketPrice] = useState([]);
   class Cats {
     constructor(name, price) {
       this.name = name;
@@ -20,12 +22,24 @@ const App = () => {
   let cat9 = new Cats("UWU", Math.floor(Math.random() * 1000));
   
   
+  const listCatNames = () => {
+    let i;
+    for (i in basketName) {
+      return(<li>{`${basketName} :`}</li>)
+    }
+  }
+
+  const listCatPrices = () => {
+    let i;
+    for (i in basketPrice) {
+      return(<li>{`£${basketPrice}`}</li>)
+    }
+  }
   
 
-  const basketName = ["Toby", "fuck you, you cunt"];
-  const basketPrice = [5767676, -33];
-  const listCatNames = basketName.map((cat) => <li id="basketList">{cat}</li>);
-  const listCatPrices = basketPrice.map((cat) => <li id="basketList">£{cat}</li>);
+  
+  // const listCatNames = basketName.map((cat) => <li id="basketList">{cat}</li>);
+  // const listCatPrices = basketPrice.map((cat) => <li id="basketList">£{cat}</li>);
   const calculateTotal = () => {
     let x = 0;
     let total = 0;
@@ -37,9 +51,43 @@ const App = () => {
     return total;
   };
 
-  const handleOnClick = () => {
-    basketName.push(`${cat1.name}`);
-    basketPrice.push(`${cat1.price}`);
+  const handleOnClick1 = () => {
+    setBasketName(basketName + cat1.name);
+    setBasketPrice(basketPrice + cat1.price);
+  };
+
+  const handleOnClick2 = () => {
+    setBasketName(basketName + cat2.name);
+    setBasketPrice(basketPrice + cat2.price);
+  };
+
+  const handleOnClick3 = () => {
+    setBasketName(basketName + cat3.name);
+    setBasketPrice(basketPrice + cat3.price);
+  };
+  const handleOnClick4 = () => {
+    setBasketName(basketName + cat4.name);
+    setBasketPrice(basketPrice + cat4.price);
+  };
+  const handleOnClick5 = () => {
+    setBasketName(basketName + cat5.name);
+    setBasketPrice(basketPrice + cat5.price);
+  };
+  const handleOnClick6 = () => {
+    setBasketName(basketName + cat6.name);
+    setBasketPrice(basketPrice + cat6.price);
+  };
+  const handleOnClick7 = () => {
+    setBasketName(basketName + cat7.name);
+    setBasketPrice(basketPrice + cat7.price);
+  };
+  const handleOnClick8 = () => {
+    setBasketName(basketName + cat8.name);
+    setBasketPrice(basketPrice + cat8.price);
+  };
+  const handleOnClick9 = () => {
+    setBasketName(basketName + cat9.name);
+    setBasketPrice(basketPrice + cat9.price);
   };
 
 
@@ -50,16 +98,16 @@ const App = () => {
         <p className="subHead">
           Your one stop shop for collecting Cats. They're all purrrrrrfect.
         </p>
-
         <div className="dropdown">
           <button>
             Basket
           </button>
           <div className="dropdown-content">
           <h1>Your Basket</h1>
-          <p>{listCatNames}</p>
-          <p>{listCatPrices}</p>
-          <p>Total price: {calculateTotal()}</p>
+          <ul>{listCatNames()}</ul>
+          <ul>{listCatPrices()}</ul>
+          {/* <p>{listCatPrices}</p> */}
+          <p>Total price: £{calculateTotal()}</p>
         </div>
         </div>
 
@@ -69,38 +117,54 @@ const App = () => {
           <h3>{cat1.name}</h3>
           <Cat />
           <p>£{cat1.price}</p>
-          <button onClick={handleOnClick}>Add to Basket</button>
+          <button onClick={handleOnClick1}>Add to Basket</button>
         </div>
         <div className="catPage">
           <h3>{cat2.name}</h3>
           <Cat />
+          <p>£{cat2.price}</p>
+          <button onClick={handleOnClick2}>Add to Basket</button>
         </div>
         <div className="catPage">
           <h3>{cat3.name}</h3>
           <Cat />
+          <p>£{cat3.price}</p>
+          <button onClick={handleOnClick3}>Add to Basket</button>
         </div>
         <div className="catPage">
           <h3>{cat4.name}</h3>
           <Cat />
+          <p>£{cat4.price}</p>
+          <button onClick={handleOnClick4}>Add to Basket</button>
         </div>
         <div className="catPage">
           <h3>{cat5.name}</h3>
           <Cat />
+          <p>£{cat5.price}</p>
+          <button onClick={handleOnClick5}>Add to Basket</button>
         </div>
         <div className="catPage">
           <h3>{cat6.name}</h3>
           <Cat />
+          <p>£{cat6.price}</p>
+          <button onClick={handleOnClick6}>Add to Basket</button>
         </div>
         <div className="catPage">
           <h3>{cat7.name}</h3>
           <Cat />
+          <p>£{cat7.price}</p>
+          <button onClick={handleOnClick7}>Add to Basket</button>
         </div>
         <div className="catPage">
           <h3>{cat8.name}</h3>
           <Cat />
+          <p>£{cat8.price}</p>
+          <button onClick={handleOnClick8}>Add to Basket</button>
         </div>
         <div className="catPage">
           <h3>{cat9.name}</h3>
+          <p>£{cat9.price}</p>
+          <button onClick={handleOnClick9}>Add to Basket</button>
           <Cat />
         </div>
       </div>
