1d38a56be2fcae6c17d522ce620709dc030687aa
Luke Ellwood <ellwood.luke@gmail.com>
2021-05-14 14:52:56 +0100

    removed unwanted code/files

--- src/Cat.js
+++ src/Cat.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-export let basket = [];
-// let total = 0;
-
 export function Cat(props) {
   const {
     type = "",
@@ -12,21 +9,14 @@ export function Cat(props) {
     filter = "",
     width = "",
     height = "",
-    // price = Math.floor(Math.random() * 1000),
   } = props;
 
   const uniqueNum = Math.random();
   const url = `https://cataas.com/cat/${type}/says/${text}?s=${fontSize}&c=${color}&filter=${filter}&width=${width}&height=${height}&uniqueNum=${uniqueNum}`;
 
-  // const handleBasket = () => {
-  //   basket.push(`${price}`);
-  // };
-
   return (
     <>
       <img src={url} alt="Cat" />
-      {/* <p>£{price}</p> */}
-      {/* <button onClick={handleBasket}>Add to Basket</button> */}
     </>
   );
 }
